177806f97b7970c780a39e2b576e3701d840138f
SaurabhNair239 <58506467+SaurabhNair239@users.noreply.github.com>
2020-09-24 18:17:30 +0530

    Update Untitled5.py

--- Untitled5.py
+++ Untitled5.py
@@ -7,6 +7,7 @@
 import pandas as pd
 import numpy as np
 import os
+
 import matplotlib.pyplot as plt
 get_ipython().run_line_magic('matplotlib', 'inline')
 
@@ -15,7 +16,7 @@ get_ipython().run_line_magic('matplotlib', 'inline')
 
 
 titantic_train = pd.read_csv("https://gist.githubusercontent.com/michhar/2dfd2de0d4f8727f873422c5d959fff5/raw/ff414a1bcfcba32481e4d4e8db578e55872a2ca1/titanic.csv",
-                             sep='/t')
+                             sep='\t')
 titantic_train
 
 
